cff135fc465c804814fa1ea8a4d418497afb2f2f
Deepthi Sandeep <deepththampi@hotmail.com>
2024-12-06 14:28:12 -0600

    Updated 012 yaml

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,18 +1,16 @@
 import models from '../models/index.js';
-import db from '../config/connection.js';  
+import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    //changed here as there was en error in tje origonal code
-    let modelExists = await models[modelName]?.db?.db?.listCollections({
+    let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
     }).toArray()
 
-    if (modelExists?.length) {
+    if (modelExists.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
     throw err;
   }
 }
-
