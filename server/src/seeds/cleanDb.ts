56e7996233dddaa3532c22b87cde63105b8cc73b
Deepthi Sandeep <deepththampi@hotmail.com>
2024-12-06 13:59:48 -0600

    Updated cleanDb

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,8 +1,9 @@
 import models from '../models/index.js';
-import db from '../config/connection.js';
+import db from '../config/connection.js';  
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
+    //changed here as there was en error in tje origonal code
     let modelExists = await models[modelName]?.db?.db?.listCollections({
       name: collectionName
     }).toArray()
